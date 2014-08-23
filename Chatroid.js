5354fcf1bb0d99200ec76bf20ecd691bad41ac34
Tsuguya Touma <touma@ryukyu-i.co.jp>
2014-08-23 16:10:27 +0900

    曜日追加

--- Chatroid.js
+++ Chatroid.js
@@ -7,20 +7,28 @@ if (Meteor.isClient) {
   };
 
     Template.login.events({
+
         'click #submit': function (e) {
 
-            Message.insert({
+            var insert = {
                 room_id:$('#room_id').val(),
                 content:$('#content').val(),
                 post_hour:$('#post_hour').val(),
                 post_minites:$('#post_minites').val(),
                 post_week:$('#post_week').val(),
                 created: new Date()
-            });
+            };
+
+            for(var i in insert) {
+                if(insert[i] == '') return;
+            }
+
+            Message.insert(insert);
+            $('#room_id').val('');
             $('#content').val('');
             $('#post_hour').val('');
             $('#post_minites').val('');
-            $('#post_week').val('');
+            $('#post_week').val('0');
         }
     });
 
@@ -31,7 +39,26 @@ if (Meteor.isClient) {
     });
 
     Template.messages.messages = function() {
-        return Message.find({},{sort:{ created: -1 }});
+        var message = Message.find({},{sort:{ created: -1 }});
+        var map = message.map(function(e){
+            console.log(e);
+           if('post_week' in e) {
+               var week = e.post_week;
+               switch(week) {
+                   case '0':week='月曜日';break;
+                   case '1':week='火曜日';break;
+                   case '2':week='水曜日';break;
+                   case '3':week='木曜日';break;
+                   case '4':week='金曜日';break;
+                   case '5':week='土曜日';break;
+                   case '6':week='日曜日';break;
+               }
+               e.post_week = week;
+               return e;
+           }
+
+        });
+        return map;
     };
 }
 
