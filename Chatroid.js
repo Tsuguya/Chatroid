402a379220219ff0844abddf818a89f112d80c6c
Tsuguya Touma <touma@ryukyu-i.co.jp>
2014-08-23 17:20:39 +0900

    fixed checkbox

--- Chatroid.js
+++ Chatroid.js
@@ -15,9 +15,13 @@ if (Meteor.isClient) {
                 content:$('#content').val(),
                 post_hour:$('#post_hour').val(),
                 post_minites:$('#post_minites').val(),
-                post_week:$('#post_week').val(),
+                post_week:[],
                 created: new Date()
             };
+
+            $('[name="post_week"]:checked').each(function(){
+                insert.post_week.push($(this).val());
+            });
 
             for(var i in insert) {
                 if(insert[i] == '') return;
@@ -28,7 +32,9 @@ if (Meteor.isClient) {
             $('#content').val('');
             $('#post_hour').val('');
             $('#post_minites').val('');
-            $('#post_week').val('0');
+            $('[name="post_week"]:checked').each(function(){
+                $(this).attr('checked', false);
+            });
         }
     });
 
@@ -42,22 +48,21 @@ if (Meteor.isClient) {
         var message = Message.find({},{sort:{ created: -1 }});
         var map = message.map(function(e){
            if('post_week' in e) {
-               var week = e.post_week;
-               switch(week) {
-                   case '0':week='月曜日';break;
-                   case '1':week='火曜日';break;
-                   case '2':week='水曜日';break;
-                   case '3':week='木曜日';break;
-                   case '4':week='金曜日';break;
-                   case '5':week='土曜日';break;
-                   case '6':week='日曜日';break;
+               var week = '';
+               for(var i in e.post_week) {
+                   switch(e.post_week[i]) {
+                       case '0':week +='日曜日';break;
+                       case '1':week +=',月曜日';break;
+                       case '2':week +=',火曜日';break;
+                       case '3':week +=',水曜日';break;
+                       case '4':week +=',木曜日';break;
+                       case '5':week +=',金曜日';break;
+                       case '6':week +=',土曜日';break;
+                   }
                }
                e.post_week = week;
-
            }
-            console.log(e);
            if('content' in e) {
-               console.log(111);
                e.content = e.content.replace(/[\n\r]/g, "<br />");
            }
            return e;
